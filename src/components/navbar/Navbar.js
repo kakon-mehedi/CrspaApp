b7bb67223285800f1e61aa22853bdab470bfad57
kakon-mehedi <kakon9000@gmail.com>
2022-10-17 13:57:28 +0600

    Add NavLink of the TodoList Navbar and Change the active link color of the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./navbar.css";
 
 function Navbar() {
-  const navigate = useNavigate();
   return (
     <>
       <div className="navbar">
         <div className="topbar">
-          <h3 className="logo" onClick={() => navigate("/")}>
+          <a href="/" className="logo">
             CrspApp
-          </h3>
-          <ul className="navbar-list">
-            <li className="navbar-item" onClick={() => navigate("/movies")}>
-              Movies
-            </li>
-            <li className="navbar-item" onClick={() => navigate("/gallery")}>
-              Gallery
-            </li>
-          </ul>
+          </a>
+
+          <nav className="navbar-list">
+            <NavLink to="/movies" className="navbar-item">
+              MovieApp
+            </NavLink>
+            <NavLink to="/gallery" className="navbar-item">
+              GalleryApp
+            </NavLink>
+          </nav>
         </div>
       </div>
     </>
